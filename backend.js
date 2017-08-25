ba69c10405438105ca64789bd05f79c4cd18e48a
ducking <kingoduck@gmail.com>
2017-08-26 03:23:48 +0800

    87% google login

--- backend.js
+++ backend.js
@@ -1,34 +1,13 @@
 var express = require('express');  
 var app = express();  
-var crypto = require('crypto');
-var jwt = require('jsonwebtoken');
-var expressJwt = require('express-jwt');
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-jwt_key = crypto.randomBytes(256);
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-checkJwt = expressJwt({
-    secret: jwt_key,
-    getToken: req => {
-        return req.cookies.token;
-    }
-});
 
-app.get('/login', function(req, res) {  
-    var token = jwt.sign({ data: 'none'}, jwt_key, {expiresIn: 60*60});
-    res.cookie('token', token).send("login ok")
-});  
-
-app.get('/', checkJwt, function(req, res) {  
-    res.send(JSON.stringify(req.user));
-});
-
-app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).send('You should <a href="login">login</a> first');
-    }
-});
+require('./src/router')(app);
 
 console.log("server listen on port 3000");
 app.listen(3000);
---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleOauthClientId: '904848968355-qqd08ni3a2pjgfe5hoc53tslnam7t12j.apps.googleusercontent.com',
+    validEmailDomain: '@nlg.csie.ntu.edu.tw',
+    loginExpirePeriod: 24 //hours
+
+}
