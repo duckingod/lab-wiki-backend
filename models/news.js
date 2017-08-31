906d17897c0a461e245cd14fc16665573b029507
vickyliin <vickyliinn@gmail.com>
2017-08-31 18:56:17 +0800

    change news content type to STRING

--- models/news.js
+++ models/news.js
@@ -3,7 +3,7 @@
 module.exports = function(sequelize, DataTypes) {
     var News = sequelize.define("News", {
           title: DataTypes.STRING,
-          content: DataTypes.BLOB,
+          content: DataTypes.STRING,
           date: DataTypes.DATE
         });
     
