e28628bee7c1076f264b6ca64e0f6802a97908a5
henryyang42 <henryyang42@gmail.com>
2017-09-05 14:24:57 +0800

    Change config

--- router/config.js
+++ router/config.js
@@ -1,25 +1,25 @@
 var env = process.env.NODE_ENV || 'development'
 config = {
   "development": {
-    "googleOauthClientId": "904848968355-qqd08ni3a2pjgfe5hoc53tslnam7t12j.apps.googleusercontent.com",
+    "googleOauthClientId": "128291458390-1rjai5msiieuad8ofmeje5eonoplsmf5.apps.googleusercontent.com",
     "validEmailDomain": "@nlg.csie.ntu.edu.tw",
     "loginExpirePeriod": 24*30, // a month
     "appUrl": "http://localhost:12345",
-    "admins": "cwtsai@nlg.csie.ntu.edu.tw pchuang@nlg.csie.ntu.edu.tw",
+    "admins": "labwiki@nlg.csie.ntu.edu.tw",
     "gpuUsage": {
-      "url": "http://localhost:28888",
-      "timeout": 3000
+      "url": "http://nlg17.csie.ntu.edu.tw:5566/",
+      "timeout": 30000
     }
   },
   "production": {
-    "googleOauthClientId": "904848968355-qqd08ni3a2pjgfe5hoc53tslnam7t12j.apps.googleusercontent.com",
+    "googleOauthClientId": "128291458390-1rjai5msiieuad8ofmeje5eonoplsmf5.apps.googleusercontent.com",
     "validEmailDomain": "@nlg.csie.ntu.edu.tw",
     "loginExpirePeriod": 24*30, // a month
     "appUrl": "http://localhost:12345",
-    "admins": "cwtsai@nlg.csie.ntu.edu.tw pchuang@nlg.csie.ntu.edu.tw",
+    "admins": "labwiki@nlg.csie.ntu.edu.tw",
     "gpuUsage": {
-      "url": "http://localhost:28888",
-      "timeout": 3000
+      "url": "http://nlg17.csie.ntu.edu.tw:5566/",
+      "timeout": 30000
     }
   }
 }
