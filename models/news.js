64d8d250101787005ec9b8f1fb7400a16de132ef
HenryYang42 <henryyang42@gmail.com>
2017-09-06 15:22:30 +0800

    Change indent in models

--- models/news.js
+++ models/news.js
@@ -1,11 +1,11 @@
 "use strict";
 
-module.exports = function(sequelize, DataTypes) {
-    var News = sequelize.define("News", {
-          title: DataTypes.STRING,
-          content: DataTypes.STRING,
-          date: DataTypes.DATE
-        });
-    
-    return News;
+module.exports = function (sequelize, DataTypes) {
+  var News = sequelize.define("News", {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    date: DataTypes.DATE
+  });
+
+  return News;
 };
